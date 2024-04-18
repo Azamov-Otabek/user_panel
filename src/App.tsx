62b4e3e1c6f659b1e47a168e159ef813c8058241
Otabek <azamov521o@gmail.com>
2024-04-19 02:16:24 +0500

    completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+const App = () => {
+  // url = "http://localhost:3000"
+  return (
+    <div>
+      <Outlet/>
+    </div>
+  );
+};
+
+export default App;
